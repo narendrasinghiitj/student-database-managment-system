776b23369a897374cb0645f15e22630c7902b205
[narendra singh] <b25ec1027@iitj.ac.in>
2026-03-18 01:24:23 +0530

    main function with mode selection

--- ics_project.c
+++ ics_project.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SUBJECTS 10
+#define MAX 100
+
+typedef struct {
+    int id;
+    char name[50];
+    int marks[SUBJECTS]; 
+    int total;
+    float percentage;
+    char grade;
+} Student;
 
 
-int main(){
-
+Student students[MAX];
+int count = 0;
+char adminPass[20] = "admin123";
+int main() {
+    loadPassword();
     
+    int choice;
+    while(1) {
+        printf("\n--- Welcome to Datanaut ---\n");
+        printf("1. Admin Mode\n2. Student Mode\n3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar(); 
 
-      int x;
-      int y;
-      int z;
-      int w;
+        switch(choice) {
+            case 1: adminMode(); break;
+            case 2: studentMode(); break;
+            case 3: saveData(); exit(0);
+            default: printf("Invalid choice! Try again.\n");
+        }
+    }
 
     return 0;
 }
