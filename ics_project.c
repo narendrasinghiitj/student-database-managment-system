425ccef57fa9165fd3a12c79f6f05e90a92bb3f1
niharika733 <b25ci1032@iitj.ac.in>
2026-04-08 18:32:04 +0530

    Grade calculation

--- ics_project.c
+++ ics_project.c
@@ -196,4 +196,17 @@ void saveData() {
 
     fclose(fp);
 }
-
+char calculateGrade(float percentage) {
+    if (percentage >= 90)
+        return 'A';
+    else if (percentage >= 80)
+        return 'B';
+    else if (percentage >= 70)
+        return 'C';
+    else if (percentage >= 60)
+        return 'D';
+    else if (percentage >= 50)
+        return 'E';
+    else
+        return 'F';
+}
